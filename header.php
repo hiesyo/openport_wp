859c3a1d38833ccb1b28a7bde3ced6d76c344753
hiesyo <hiesyo3314@yahoo.co.jp>
2018-10-03 14:27:50 +0900

    index.php、header.php作成

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="utf-8">
+<meta name="format-detection" content="telephone=no">
+<meta name="viewport" content="initial-scale=1.0">
+<?php wp_head(); ?>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+</head>
